8b359411e01b179e414b9ed3e2fcb3cbbbea38ff
Ash Rhazaly <ashleighrhazaly@gmail.com>
2017-02-17 13:17:48 +0800

    Add callback to post controller and seed data

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,20 @@
 class PostsController < ApplicationController
+  before_action :set_params, only: [:index, :new, :update, :edit, :destroy]
   def index
+    @post = Post.all
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to posts_path
+  end
+
+  def show
   end
 
   def update
@@ -15,5 +24,16 @@ class PostsController < ApplicationController
   end
 
   def destroy
+    @post.destory
+    redirect_to posts_path
+  end
+
+private
+  def set_params
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:name,:subject,:category,:content)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+posts = Post.create([{name: 'Ash', subject: 'HELP HELP THE WALL IS FALLING', category: 'Latest news',
+  content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla aliquam tempus risus,
+  quis aliquam lacus mattis eget. In id sem nec orci venenatis mollis. Ut vitae tempus tortor,
+  sed vulputate est. In convallis tincidunt mauris quis aliquet. Maecenas varius feugiat leo id
+  fermentum. Vestibulum condimentum, ligula eget ornare porta, metus ligula sagittis nunc, sed
+  varius sapien nisl vitae ligula. Vestibulum id turpis nisl.'}, {name:'Yao Min', subject: 'Over meditation',
+  category: 'Need Help', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla aliquam
+   tempus risus, quis aliquam lacus mattis eget. In id sem nec orci venenatis mollis. Ut vitae tempus
+   tortor, sed vulputate est. In convallis tincidunt mauris quis aliquet. Maecenas varius feugiat leo
+   id fermentum. Vestibulum condimentum, ligula eget ornare porta, metus ligula sagittis nunc, sed
+   varius sapien nisl vitae ligula. Vestibulum id turpis nisl.'}])

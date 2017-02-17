5dbf06a1a959c9a3fd5f0d02781f609681e412b4
Ash Rhazaly <ashleighrhazaly@gmail.com>
2017-02-17 14:09:55 +0800

    Add CRUD functionality to comment controller

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -1,11 +1,17 @@
 class CommentController < ApplicationController
+  before_action :set_params, only: [:show,:update,:edit,:destroy]
   def index
+    @comment = Comment.all
   end
 
   def new
+    @comment = Comment.new
   end
 
   def create
+    @comment = Comment.new(comment_params)
+    @comment.save
+    redirect_to comments_path
   end
 
   def show
@@ -18,5 +24,15 @@ class CommentController < ApplicationController
   end
 
   def destroy
+    @comment.destroy
+    redirect_to comments_path
+  end
+
+private
+  def set_params
+    @comment = Comment.find(params[:id])
+  end
+  def comment_params
+    params.require(:comment).permit(:name,:content)
   end
 end
